7d8a0938a4c9bb23e3ed19462193cad4b70552d8
Carmelito Jr <carmelito.benali@hotmail.com>
2024-03-16 09:42:44 -0300

    First class: Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,15 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        Position pos = new Position(3, 5);
+
+        System.out.println(pos);
+
 
 
     }
